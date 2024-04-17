6cea407732e9403694af9a213d10c8dd2d461466
hsuliz <sasha.suliz@gmail.com>
2024-04-17 21:28:20 +0200

    Added BookController try/catch

--- backend-services/book-service/src/main/kotlin/dev/hsuliz/bookservice/controller/BookController.kt
+++ backend-services/book-service/src/main/kotlin/dev/hsuliz/bookservice/controller/BookController.kt
@@ -2,11 +2,14 @@ package dev.hsuliz.bookservice.controller
 
 import dev.hsuliz.bookservice.dao.BookRequest
 import dev.hsuliz.bookservice.dao.BookResponse
+import dev.hsuliz.bookservice.exception.BookAlreadyExistsException
+import dev.hsuliz.bookservice.exception.BookNotFoundException
 import dev.hsuliz.bookservice.service.BookService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/book")
@@ -14,11 +17,21 @@ class BookController(private val bookService: BookService) {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     suspend fun createBook(@RequestBody bookRequest: BookRequest): BookResponse =
-        bookService.saveBook(bookRequest.toModel()).toResponse()
+        try {
+            bookService.saveBook(bookRequest.toModel()).toResponse()
+        } catch (exception: BookAlreadyExistsException) {
+            throw ResponseStatusException(HttpStatus.CONFLICT, exception.message)
+        }
 
     @GetMapping("/{id}")
     suspend fun getBookById(@PathVariable id: String): BookResponse =
-        bookService.getBookById(id).toResponse()
+        try {
+            bookService.getBookById(id).toResponse()
+        } catch (exception: BookNotFoundException) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, exception.message)
+        } catch (exception: IllegalArgumentException) {
+            throw ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, exception.message)
+        }
 
     @GetMapping
     fun findAllBooks(): Flow<BookResponse> = bookService.findAllBooks().map { it.toResponse() }
@@ -28,5 +41,10 @@ class BookController(private val bookService: BookService) {
         bookService.findBooksByTitle(title).map { it.toResponse() }
 
     @DeleteMapping("/{id}")
-    suspend fun deleteBookById(@PathVariable id: String) = bookService.deleteBookById(id)
+    suspend fun deleteBookById(@PathVariable id: String) =
+        try {
+            bookService.deleteBookById(id)
+        } catch (exception: BookNotFoundException) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, exception.message)
+        }
 }
