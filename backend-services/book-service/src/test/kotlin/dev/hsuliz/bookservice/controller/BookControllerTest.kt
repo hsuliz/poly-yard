6e0c206b9713994275423e0cd488fac3ce689718
hsuliz <sasha.suliz@gmail.com>
2024-04-20 19:00:57 +0200

    Added saveBookController test

--- backend-services/book-service/src/test/kotlin/dev/hsuliz/bookservice/controller/BookControllerTest.kt
+++ backend-services/book-service/src/test/kotlin/dev/hsuliz/bookservice/controller/BookControllerTest.kt
@@ -4,11 +4,14 @@ import com.ninjasquad.springmockk.MockkBean
 import dev.hsuliz.bookservice.TestConstants.NORMAL_BOOK
 import dev.hsuliz.bookservice.TestConstants.NORMAL_BOOK_REQUEST
 import dev.hsuliz.bookservice.dao.BookResponse
+import dev.hsuliz.bookservice.exception.BookAlreadyExistsException
 import dev.hsuliz.bookservice.service.BookService
 import io.kotest.core.spec.style.FunSpec
 import io.mockk.coEvery
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ProblemDetail
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @WebFluxTest(BookController::class)
@@ -18,10 +21,9 @@ class BookControllerTest(
 ) :
     FunSpec({
         context("Create book") {
-            test("Should save book") {
+            test("Should save") {
                 val givenBook = NORMAL_BOOK
                 coEvery { bookService.saveBook(any()) } returns givenBook
-
                 webClient
                     .post()
                     .uri(BOOK_URI)
@@ -32,6 +34,23 @@ class BookControllerTest(
                     .isCreated
                     .expectBody(BookResponse::class.java)
                     .isEqualTo(givenBook.toResponse())
+            }
+
+            test("Should not save") {
+                val givenBook = NORMAL_BOOK
+                coEvery { bookService.saveBook(any()) } throws
+                    BookAlreadyExistsException(
+                        "Book with title [${givenBook.title}] already exists"
+                    )
+                webClient
+                    .post()
+                    .uri(BOOK_URI)
+                    .bodyValue(NORMAL_BOOK_REQUEST)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .exchange()
+                    .expectStatus()
+                    .isEqualTo(HttpStatus.CONFLICT)
+                    .expectBody(ProblemDetail::class.java)
             }
         }
     })
