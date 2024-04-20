9d12bea8c94b125c7f8fc6d76d9653bdc89f7f51
hsuliz <sasha.suliz@gmail.com>
2024-04-20 19:08:59 +0200

    Added getBookById test

--- backend-services/book-service/src/test/kotlin/dev/hsuliz/bookservice/controller/BookControllerTest.kt
+++ backend-services/book-service/src/test/kotlin/dev/hsuliz/bookservice/controller/BookControllerTest.kt
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ProblemDetail
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
 
 @WebFluxTest(BookController::class)
 class BookControllerTest(
@@ -32,7 +33,7 @@ class BookControllerTest(
                     .exchange()
                     .expectStatus()
                     .isCreated
-                    .expectBody(BookResponse::class.java)
+                    .expectBody<BookResponse>()
                     .isEqualTo(givenBook.toResponse())
             }
 
@@ -50,7 +51,23 @@ class BookControllerTest(
                     .exchange()
                     .expectStatus()
                     .isEqualTo(HttpStatus.CONFLICT)
-                    .expectBody(ProblemDetail::class.java)
+                    .expectBody<ProblemDetail>()
+            }
+        }
+        context("Get book by id") {
+            test("Should return") {
+                val givenBook = NORMAL_BOOK
+                coEvery { bookService.getBookById(any()) } returns givenBook
+
+                webClient
+                    .get()
+                    .uri("$BOOK_URI/{id}", givenBook.id)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .exchange()
+                    .expectStatus()
+                    .isOk
+                    .expectBody<BookResponse>()
+                    .isEqualTo(givenBook.toResponse())
             }
         }
     })
