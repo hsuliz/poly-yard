8401625729777cb612b4bc8212634335ff64de47
hsuliz <sasha.suliz@gmail.com>
2024-04-21 12:01:50 +0200

    Added test Get book by id Should not found

--- backend-services/book-service/src/test/kotlin/dev/hsuliz/bookservice/controller/BookControllerTest.kt
+++ backend-services/book-service/src/test/kotlin/dev/hsuliz/bookservice/controller/BookControllerTest.kt
@@ -5,11 +5,13 @@ import dev.hsuliz.bookservice.TestConstants.NORMAL_BOOK
 import dev.hsuliz.bookservice.TestConstants.NORMAL_BOOK_REQUEST
 import dev.hsuliz.bookservice.dao.BookResponse
 import dev.hsuliz.bookservice.exception.BookAlreadyExistsException
+import dev.hsuliz.bookservice.exception.BookNotFoundException
 import dev.hsuliz.bookservice.service.BookService
 import io.kotest.core.spec.style.FunSpec
 import io.mockk.coEvery
+import org.bson.types.ObjectId
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
-import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.*
 import org.springframework.http.MediaType
 import org.springframework.http.ProblemDetail
 import org.springframework.test.web.reactive.server.WebTestClient
@@ -36,7 +38,7 @@ class BookControllerTest(
                 request
                     .exchange()
                     .expectStatus()
-                    .isCreated
+                    .isEqualTo(CREATED)
                     .expectBody<BookResponse>()
                     .isEqualTo(givenBook.toResponse())
             }
@@ -48,27 +50,38 @@ class BookControllerTest(
                         "Book with title [${givenBook.title}] already exists"
                     )
 
-                request
-                    .exchange()
-                    .expectStatus()
-                    .isEqualTo(HttpStatus.CONFLICT)
-                    .expectBody<ProblemDetail>()
+                request.exchange().expectStatus().isEqualTo(CONFLICT).expectBody<ProblemDetail>()
             }
         }
         context("Get book by id") {
+            val request = { bookId: String ->
+                webClient.get().uri("$BOOK_URI/{id}", bookId).accept(MediaType.APPLICATION_JSON)
+            }
+
             test("Should return") {
                 val givenBook = NORMAL_BOOK
+                val givenBookId = ObjectId.get().toHexString()
                 coEvery { bookService.getBookById(any()) } returns givenBook
 
-                webClient
-                    .get()
-                    .uri("$BOOK_URI/{id}", givenBook.id)
-                    .accept(MediaType.APPLICATION_JSON)
+                request(givenBookId)
                     .exchange()
                     .expectStatus()
-                    .isOk
+                    .isEqualTo(OK)
                     .expectBody<BookResponse>()
                     .isEqualTo(givenBook.toResponse())
+            }
+
+            test("Should not found") {
+                val givenBookId = ObjectId.get().toHexString()
+
+                coEvery { bookService.getBookById(any()) } throws
+                    BookNotFoundException("Book with id:[$givenBookId] not found")
+
+                request(givenBookId)
+                    .exchange()
+                    .expectStatus()
+                    .isEqualTo(NOT_FOUND)
+                    .expectBody<ProblemDetail>()
             }
         }
     })
