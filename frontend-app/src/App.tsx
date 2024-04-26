dc2a1c4f6d54ce516177888fc1cbc0426344723b
hsuliz <sasha.suliz@gmail.com>
2024-04-26 12:23:19 +0200

    Added naive Book component

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -3,7 +3,7 @@ import React from "react"
 function App() {
   return (
     <div>
-      <p>Hello!</p>
+      <h1>Poly-yard</h1>
     </div>
   )
 }
--- frontend-app/src/book/Book.tsx
+++ frontend-app/src/book/Book.tsx
@@ -1,15 +1,50 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import findAllBooks from "../api/BookApi"
+import { BookResponse } from "./BookResponse"
 
 const Book = () => {
+  const [books, setBooks] = useState<Array<BookResponse>>([])
+
   useEffect(() => {
-    findAllBooks.then((value) => console.log(value))
+    findAllBooks
+      .then((res) => res.data)
+      .then((data) => {
+        setBooks(data)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
   }, [])
 
   return (
     <div>
-      <h1>Welcome to my app</h1>
+      <p>Books</p>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Rating</th>
+            <th>Comment</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book, index) => (
+            <tr key={book.id}>
+              <td>{index + 1}</td>
+              <td>{book.title}</td>
+              <td>
+                {book.author.firstName} {book.author.firstName}
+              </td>
+              <td>{book.review.rating}</td>
+              <td>{book.review.comment}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
+
 export default Book
