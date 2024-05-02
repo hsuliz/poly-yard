5765b0cc491a52ce67500279eaab166374b6a243
hsuliz <sasha.suliz@gmail.com>
2024-05-02 12:57:52 +0200

    Removed book from Router.tsx

--- frontend-app/src/Router.tsx
+++ frontend-app/src/Router.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom"
-import Book from "./book/Book"
 import BookDetails from "./book/BookDetails"
 import React from "react"
 import App from "./App"
@@ -7,7 +6,6 @@ import App from "./App"
 const Router = () => (
   <Routes>
     <Route path="/" element={<App />} />
-    <Route path="/book" element={<Book />} />
     <Route path="/book/:bookId" element={<BookDetails />} />
   </Routes>
 )
