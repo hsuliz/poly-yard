72552859f1506fd38c09cf174ed0381043cfd143
hsuliz <sasha.suliz@gmail.com>
2024-05-05 08:36:34 +0200

    Refactored App

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -4,17 +4,15 @@ import Home from "./home/Home"
 import { Route, Routes } from "react-router-dom"
 import BookDetails from "./book/BookDetails"
 
-function App() {
-  return (
-    <div className="bg-gray-900 h-screen w-screen">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/book/:bookId" element={<BookDetails />} />
-        </Route>
-      </Routes>
-    </div>
-  )
-}
+const App = () => (
+  <div className="bg-gray-900 h-screen w-screen">
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/book/:bookId" element={<BookDetails />} />
+      </Route>
+    </Routes>
+  </div>
+)
 
 export default App
