eb24da7b1b57b203286fd116817afa826bf1daa6
hsuliz <sasha.suliz@gmail.com>
2024-09-12 12:03:49 +0200

    Refactored

--- backend-services/book-service/src/main/kotlin/dev/hsuliz/bookservice/book/component/BookSearcher.kt
+++ backend-services/book-service/src/main/kotlin/dev/hsuliz/bookservice/book/component/BookSearcher.kt
@@ -15,43 +15,42 @@ import org.springframework.web.reactive.function.client.awaitBody
 @Component
 class BookSearcher(private val bookInfoClient: WebClient) {
 
-    suspend fun findBookByIsbn(isbn: String): Book {
-        val validIsbn = getValidIsbn(isbn)
-        return jacksonObjectMapper()
-            .apply { registerModule(JavaTimeModule()) }
-            .readValue<BookMapper>(
-                bookInfoClient.get().uri("/$validIsbn").retrieve().awaitBody<String>()
-            )
-            .bookInfo
-            .toBookModel()
-    }
+  suspend fun findBookByIsbn(isbn: String): Book {
+    val validIsbn = getValidIsbn(isbn)
+    return jacksonObjectMapper()
+        .apply { registerModule(JavaTimeModule()) }
+        .readValue<BookMapper>(
+            bookInfoClient.get().uri("/$validIsbn").retrieve().awaitBody<String>())
+        .bookInfo
+        .toBookModel()
+  }
 
-    private suspend fun getValidIsbn(isbnToValidate: String): String {
-        when {
-            isbnToValidate.matches(Regex("\\b(?:\\d{9}(\\d|X)|\\d{13})\\b")) -> return isbnToValidate
-            else -> throw Exception("Wrong isbn") // todo create exception
-        }
+  private suspend fun getValidIsbn(isbnToValidate: String): String {
+    when {
+      isbnToValidate.matches(Regex("\\b(?:\\d{9}(\\d|X)|\\d{13})\\b")) -> return isbnToValidate
+      else -> throw Exception("Wrong isbn") // todo create exception
     }
+  }
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 private data class BookMapper(@JsonProperty("book") val bookInfo: BookInfo) {
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    data class BookInfo(
-        val isbn13: String,
-        val title: String,
-        val author: List<String>,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-        val publishedDate: LocalDate,
-        val numberOfPages: Int,
-        var image: String
-    ) {
-        init {
-            image = image.replace(Regex("._SL[0-9]+_"), "")
-        }
-
-        fun toBookModel(): Book {
-            return Book(isbn13, title, author[0], publishedDate.year, numberOfPages, image)
-        }
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  data class BookInfo(
+      val isbn13: String,
+      val title: String,
+      val author: List<String>,
+      @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+      val publishedDate: LocalDate,
+      val numberOfPages: Int,
+      var image: String
+  ) {
+    init {
+      image = image.replace(Regex("._SL[0-9]+_"), "")
     }
+
+    fun toBookModel(): Book {
+      return Book(isbn13, title, author[0], publishedDate.year, numberOfPages, image)
+    }
+  }
 }
