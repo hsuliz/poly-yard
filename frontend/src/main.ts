b8921e2aa5471a9218f6771667a061734c1a96ab
hsuliz <hlib.suliz@gmail.com>
2024-11-25 19:14:29 +0100

    Removed pinia

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,5 @@
 import "./index.css"
 import { createApp } from "vue"
-import { createPinia } from "pinia"
 import { vueKeycloak } from "@josempgon/vue-keycloak"
 
 import App from "./App.vue"
@@ -8,7 +7,6 @@ import router from "./router"
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 
 app.use(vueKeycloak, {
