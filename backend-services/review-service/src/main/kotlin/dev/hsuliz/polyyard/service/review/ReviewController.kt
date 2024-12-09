af3e9aab3c4257bd5a1c0248fb541e6500c0d0f6
hsuliz <hlib.suliz@gmail.com>
2024-12-09 11:59:12 +0100

    Added repository based on r2dbc template

--- backend-services/review-service/src/main/kotlin/dev/hsuliz/polyyard/service/review/ReviewController.kt
+++ backend-services/review-service/src/main/kotlin/dev/hsuliz/polyyard/service/review/ReviewController.kt
@@ -2,6 +2,7 @@ package dev.hsuliz.polyyard.service.review
 
 import dev.hsuliz.polyyard.service.review.dto.ReviewRequest
 import dev.hsuliz.polyyard.service.review.dto.ReviewResponse
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
@@ -18,13 +19,31 @@ class ReviewController(private val reviewService: ReviewService) {
 
   @GetMapping("/reviews")
   suspend fun findReviews(
+      @RequestParam(value = "username", required = false) username: String?,
+      @RequestParam(value = "resource-type", required = false) resourceType: Review.Resource.Type?,
+      @RequestParam(value = "resource-value", required = false) resourceValue: String?,
       @PageableDefault(page = 0, size = 10, sort = ["createdAt"], direction = Sort.Direction.DESC)
       pageable: Pageable
   ): Page<ReviewResponse> {
-    val reviews = reviewService.findReviews(pageable).toList()
+    val reviews =
+        reviewService.findReviews(username, resourceType, resourceValue, pageable).toList()
     val response = reviews.map { ReviewResponse(it) }
     return PageImpl(response, pageable, reviewService.countReviews())
   }
+
+  // @GetMapping("/reviews/test")
+  // suspend fun findReviewsByResource(
+  //    @RequestParam(value = "resource-type", required = false) resourceType:
+  // Review.Resource.Type?,
+  //    @RequestParam(value = "resource-value", required = false) resourceValue: String?,
+  //    @PageableDefault(page = 0, size = 10, sort = ["createdAt"], direction = Sort.Direction.DESC)
+  //    pageable: Pageable
+  // ): Page<ReviewResponse> {
+  //  val reviews =
+  //      reviewService.findReviewsTest(resourceType, resourceValue, pageable).toList()
+  //  val response = reviews.map { ReviewResponse(it) }
+  //  return PageImpl(response, pageable, 2)
+  // }
 
   @GetMapping("/reviews/{username}")
   suspend fun findReviewsByUsername(
