19c3d9f47d1cd82ea829004ba0ab1869286a61e9
hsuliz <hlib.suliz@gmail.com>
2024-12-23 19:45:36 +0100

    Removed transaction in test

--- backend-services/review-service/src/test/kotlin/dev/hsuliz/polyyard/service/review/service/ReviewServiceIntegrationTest.kt
+++ backend-services/review-service/src/test/kotlin/dev/hsuliz/polyyard/service/review/service/ReviewServiceIntegrationTest.kt
@@ -10,6 +10,7 @@ import dev.hsuliz.polyyard.service.review.model.Review
 import dev.hsuliz.polyyard.service.review.util.currentUsername
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.extensions.spring.SpringExtension
 import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
 import io.mockk.every
@@ -24,12 +25,11 @@ import org.springframework.data.domain.ReactiveAuditorAware
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
 import org.springframework.data.relational.core.query.Criteria
 import org.springframework.data.relational.core.query.Query
+import org.springframework.r2dbc.core.await
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
-import org.springframework.transaction.annotation.Transactional
 import reactor.core.publisher.Mono
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Transactional
 class ReviewServiceIntegrationTest(
     private val reviewService: ReviewService,
     private val r2dbcEntityTemplate: R2dbcEntityTemplate,
@@ -41,6 +41,51 @@ class ReviewServiceIntegrationTest(
     PostgresTestcontainer,
     RabbitMQTestcontainer,
     FunSpec({
+      beforeEach {
+        r2dbcEntityTemplate.databaseClient
+            .sql(
+                """
+                DELETE FROM reviews CASCADE
+            """)
+            .await()
+        r2dbcEntityTemplate.databaseClient
+            .sql(
+                """
+                DELETE FROM resources CASCADE
+            """)
+            .await()
+
+        r2dbcEntityTemplate.databaseClient
+            .sql("ALTER SEQUENCE reviews_id_seq RESTART WITH 1")
+            .await()
+
+        r2dbcEntityTemplate.databaseClient
+            .sql("ALTER SEQUENCE resources_id_seq RESTART WITH 1")
+            .await()
+
+        r2dbcEntityTemplate.databaseClient
+            .sql(
+                """
+            INSERT INTO resources (type, value) VALUES
+            ('ISBN', '9783161484100'),
+            ('ISBN', '9780306406157'),
+            ('ISBN', '9781451673319'),
+            ('ISBN', '9780140449136')
+         """)
+            .await()
+
+        r2dbcEntityTemplate.databaseClient
+            .sql(
+                """
+            INSERT INTO reviews (username, type, resource_id, rating, comment) VALUES
+            ('user1', 'BOOK', 1, 5, 'Fantastic book! A must-read.'),
+            ('user2', 'BOOK', 2, 4, 'Great concepts but a bit verbose.'),
+            ('user3', 'BOOK', 3, 5, 'Incredible storytelling.'),
+            ('user4', 'BOOK', 4, 3, 'Good, but not my style.')
+         """)
+            .await()
+      }
+
       test("Should save review for new resource and send it to MQ") {
         // setup
         Queue("book").also { amqpAdmin.declareQueue(it) }
@@ -125,4 +170,6 @@ class ReviewServiceIntegrationTest(
         val x = reviewService.findReviews()
         x.toList().forEach { println(it) }
       }
-    })
+    }) {
+  override fun extensions() = listOf(SpringExtension)
+}
